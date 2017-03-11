a555bb2ca3e73f468e6915e49691b16a10b3d7ff
Mathew C Martin <chactas.stryker@gmail.com>
2017-03-10 20:35:39 -0800

    Trying to git it!

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+var color = prompt('What is your favorite color?');
+alert('I like ' + color + ' too.');
+console.log(color);
+var flavor = prompt('What is your favorite flavor');
+alert('I like ' + flavor + ' too.');
+console.log(flavor);
+var hobby = prompt('What is your favorite hobby?');
+alert('I like ' + hobby + ' too.');
+console.log(hobby);
+var sport = prompt('What is your favorite sport?');
+alert('Wow, I like all your choices!' + ' ' + color + ' ' + flavor + ' ' + hobby + ' ' + sport );
+console.log(sport);
--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -29,11 +29,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var mult = a * b;
+  var msg1 = 'The product of ' + a + ' and ' + b + ' is ' + mult + '.';
+  return [mult, msg1];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -50,11 +52,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var newAdd = (add,c);
+  var newMultiply = (multiply,c);
+  var sumString = a + ' and ' + b + ' and ' + c + ' to ' + sum + '.';
+  var productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply + '.';
+  return [sum, product, sumString, productString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
